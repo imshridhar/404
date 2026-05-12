46fae59851f21f92083b1c6cb3b34b99c8b56f63
imshridhar <shridharhavinal@gmail.com>
2026-05-12 06:59:38 +0530

    hey

--- React/exp9/src/App.jsx
+++ React/exp9/src/App.jsx
@@ -1,13 +1,33 @@
-import Nav from "./Nav"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-function App() {
-  
-
-  return (
-    <>
-     <Nav/>
-    </>
-  )
+function Home() {
+  return <h1>Home Page</h1>;
 }
 
-export default App
+function About() {
+  return <h1>About Page</h1>;
+}
+
+function Contact() {
+  return <h1>Contact Page</h1>;
+}
+
+export default function App() {
+  return (
+    <BrowserRouter>
+
+      <nav>
+        <Link to="/">Home </Link>
+        <Link to="/about">About </Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
+    </BrowserRouter>
+  );
+}
